999a1834d8cc274322af33b96b99a91d8233ea0c
user <user@DESKTOP-CD9F5VF>
2022-06-22 14:32:45 -0700

    Add project files.

---
+++ AllowCompSpawn/AllowCompSpawn.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+
+namespace AllowCompSpawn
+{
+    public class AllowCompSpawn : NeosMod
+    {
+        public override string Name => "AllowCompSpawn";
+        public override string Author => "eia485";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/EIA485/NeosAllowCompSpawn/";
+        
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.eia485.allowCompSpawn");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(ReferenceProxy), "ShouldBeProtected")]
+        private class allowCompSpawnPatch
+        {
+            public static bool Prefix(ref bool __result)
+            {
+                __result = false;
+                return false;
+            }
+        }
+    }
+}
